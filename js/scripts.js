fbe375e1dc0d3e263795a43a4e51ebd2aa6f83c4
Paulina Sikora <paaulinaaas@gmail.com>
2017-07-14 16:45:56 +0200

    zadanie 6

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+// pobranie referencji do elementu przycisku, listy, oraz wszystkich
+// elementów li znajdujących się na stronie
+var addBtn = document.getElementById('addElem'),
+    list = document.getElementById('list'),
+    liElements = document.getElementsByTagName('li');
+
+// dodanie obsługi zdarzenia kliknięcia w przycisk
+addBtn.addEventListener('click', function() {
+  // utworzenie nowego elementu li
+  var newListElement = document.createElement('li');
+  // nadanie wartości innerText temu elementu
+  newListElement.innerText = 'item ' + liElements.length;
+  
+  // dodanie nowo utworzonego elementu do listy
+  list.appendChild(newListElement);
